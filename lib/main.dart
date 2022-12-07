b8e7304a63d5a1b27dabfe6ef764ed2d23c56e1b
jonnahfreire <jonnas.freire17@gmail.com>
2022-12-06 21:23:56 -0300

    Add firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:uru_bank/firebase_options.dart';
 import 'package:uru_bank/routes/app_routes.dart';
 import 'package:uru_bank/utils/styles.dart';
+import 'package:provider/provider.dart';
+import 'package:uru_bank/providers/user.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
@@ -11,14 +21,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: AppRoutes.routes,
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          backgroundColor: Styles.appBackgroundColor,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<UserData>(
+          create: (context) => UserData(),
         ),
+      ],
+      child: MaterialApp(
+        routes: AppRoutes.routes,
+        theme: ThemeData(
+          appBarTheme: AppBarTheme(
+            backgroundColor: Styles.appBackgroundColor,
+          ),
+        ),
+        debugShowCheckedModeBanner: false,
       ),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
